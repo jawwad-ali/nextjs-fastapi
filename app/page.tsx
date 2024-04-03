1d5371ef76927c2bf24604fef69330741c47a280
jawwad-ali <alijawwad001@gmail.com>
2024-04-04 01:49:26 +0500

    made client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { addTodos } from "@/app/server/actions";
 import DeleteButton, { UpdateCheckBox } from "./components/DeleteButton";
 import { Input } from "@/components/ui/input";
